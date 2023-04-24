3aa6e8c421dc3430e2ed77f97a5b342de2965c8c
ElevenNotes <github@11notes.ch>
2023-04-24 23:22:14 +0200

    move volumes to /mirror from /nginx

--- amd64.dockerfile
+++ amd64.dockerfile
@@ -6,12 +6,23 @@
 
 	# :: prepare
 		RUN set -ex; \
+      mkdir -p /mirror/etc; \
+      mkdir -p /mirror/var; \
 			apk add --update --no-cache \
         rsync \
         mqtt-exec;
 
-    # :: copy root filesystem changes
-      ADD --chown=1000:1000 ./rootfs /
-      RUN set -ex; chmod +x -R /usr/local/bin
+  # :: copy root filesystem changes
+    ADD ./rootfs /
+    RUN set -ex; chmod +x -R /usr/local/bin
 
+	# :: docker -u 1000:1000 (no root initiative)
+		RUN set -ex; \
+			chown nginx:nginx -R \
+				/mirror;
+
+# :: Volumes
+	VOLUME ["/mirror/etc", "/mirror/var"]
+
+# :: Start
   USER nginx
--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -1,4 +1,33 @@
 #!/bin/ash
+  tee /nginx/etc/default.conf <<EOF
+log_format proxy escape=json '{"destination":"remote", "client":"\$http_x_forwarded_for", "url":"\$request_uri", "status":\$status}';
+log_format local escape=json '{"destination":"local", "client":"\$http_x_forwarded_for", "url":"\$request_uri", "status":\$status}';
+
+server {
+  listen 8080 default_server;
+  server_name _;
+  root /mirror/var;
+  autoindex on;
+
+  error_log off;
+  access_log /var/log/nginx/access.log local;
+
+  location / {
+    access_log /var/log/nginx/access.log proxy;
+    resolver 208.67.222.222 208.67.220.220;
+    proxy_pass http://dl-cdn.alpinelinux.org/alpine\$request_uri;
+  } 
+
+  location /favicon.ico {
+    try_files $uri favicon.ico;
+  }
+
+  %INCLUDE%
+
+  include /mirror/etc/*.conf;
+}
+EOF
+
   NGINX_INCLUDE=""
 
   for VERSION in "$@"; do
